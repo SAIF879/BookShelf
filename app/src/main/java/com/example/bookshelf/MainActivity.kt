f8366d93a3c74705fc4840fe0c9d9744b81d1dc2
SAIF879 <saifahmed9308@gmail.com>
2023-05-06 00:49:16 +0530

    firebase testing done

--- app/src/main/java/com/example/bookshelf/MainActivity.kt
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bookshelf
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,16 +12,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookshelf.ui.theme.BookShelfTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookShelfTheme {
-                Surface(
+
+//                val db = FirebaseFirestore.getInstance()
+//                val user: MutableMap<String, Any> = HashMap()
+//                user["firstName"] = "Joe"
+//                user["lastName"] = "mama"
+
+
+
+                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+
+//                     db.collection("users").add(user)
+//                         .addOnSuccessListener { Log.d("FB", "onCreate: ${it.id}") }
+//                         .addOnFailureListener {
+//                             Log.d("FB", "onCreate: $it")
+//                         }
 
                 }
             }
